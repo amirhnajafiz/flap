cc3394b8700f61bdcdbf4915b5eacae0664791ed
amirhnajafiz <najafizadeh21@gmail.com>
2026-01-09 13:12:44 -0500

    update: toyes dest in tests

--- tests/toys_test.sh
+++ tests/toys_test.sh
@@ -7,7 +7,11 @@ make all
 cd ../..
 
 # run the tracer on the copy toy
-PYTHONPATH=. python3 entrypoint/app.py -ex "./tests/toys/bin/copy ./tests/toys/src/copy.c ./tests/toys/bin/copied.c" --debug --out "/tmp/flak-toy-copy"
+PYTHONPATH=. python3 entrypoint/app.py -ex "./tests/toys/bin/copy ./tests/toys/src/copy.c /tmp/copied.c" --debug --out "/tmp/flak-toy-copy"
 
 # run the tracer on the mmap toy
-PYTHONPATH=. python3 entrypoint/app.py -ex "./tests/toys/bin/mmap ./tests/toys/src/mmap.c ./tests/toys/bin/mapped.c" --debug --out "/tmp/flak-toy-mmap"
+PYTHONPATH=. python3 entrypoint/app.py -ex "./tests/toys/bin/mmap ./tests/toys/src/mmap.c /tmp/mapped.c" --debug --out "/tmp/flak-toy-mmap"
+
+# cleanup
+rm -f /tmp/copied.c
+rm -f /tmp/mmaped.c
